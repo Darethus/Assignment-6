239c7fa040cf38810fd9b68afadeff6aab3f5223
elev <elev@S150924-HP002>
2017-11-13 15:44:48 +0100

    aaa

---
+++ Assignment6/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Assignment6.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        
+        public int ClassID { get; set; }
+
+        [ForeignKey("ClassID")]
+        public virtual Class Class { get; set; }
+
+
+    }
+}
---
+++ Assignment6/Models/Teacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment6.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int TeacherID { get; set; }
+        public string TeacherName { get; set; }
+
+
+        public virtual ICollection<Class> Classes { get; set; }
+
+    }
+}
